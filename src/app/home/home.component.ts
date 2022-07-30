799b1a13e2f95397c7bcfa0485ad86724daede84
hvchunghp <89526024+hvchunghp@users.noreply.github.com>
2022-07-30 10:29:37 +0700

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent implements OnInit {
   constructor() {}
   user = [
     {
-      avt: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE-IaGgfx8si0t9VePECDdAQdnb0D1z8HgUXjBQfpn-smN_ZzIzY_wGe-eCZ-TKW_hHHE&usqp=CAU',
+      avt: 'https://scr.vn/wp-content/uploads/2020/07/%E1%BA%A2nh-%C4%91%E1%BA%A1i-di%E1%BB%87n-FB-m%E1%BA%B7c-%C4%91%E1%BB%8Bnh-n%E1%BB%AF.jpg',
       name: 'Nguyễn Thị Kim Chi',
       subject: 'Apology for late response email',
       mess: 'Hello Chung,',
